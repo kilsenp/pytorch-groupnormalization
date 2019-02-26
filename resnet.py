0e699ffe7ed95b234f08c62bca4b6592cfd15dbf
Kilian Pfeiffer <kilian.pfeiffer@rwth-aachen.de>
2019-02-26 11:21:17 +0100

    Make module.

--- resnet.py
+++ resnet.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import math
 import torch.utils.model_zoo as model_zoo
-from group_norm  import GroupNorm2d
+from .group_norm  import GroupNorm2d
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
            'resnet152']
